00bea47c390aeb2ec7624a2898f476a9f182a630
Logan <lrl2777@g.rit.edu>
2025-10-05 01:17:06 -0400

    Remade proj and start of assignment 3

---
+++ NewProj/IGME689/Assets/Scripts/FeatureTrackCreator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script that creates a race track of splines based off of points gotten from feature layer API
+/// </summary>
+public class FeatureTrackCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
